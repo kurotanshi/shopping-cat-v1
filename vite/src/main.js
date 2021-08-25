7f2cb5c705da4243de37c202d47a6a3ee85c4216
Kuro Hsu <kurotanshi@gmail.com>
2021-08-25 19:27:11 +0800

    vite init

---
+++ vite/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "bootstrap/dist/css/bootstrap.css"
+import "@fortawesome/fontawesome-free/css/all.css"
+import './style.scss';
+
+createApp(App).mount('#app')
